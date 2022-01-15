159931d0bf4721725f5e00bdcd682c4675d1fc61
Bohdan Hrynkiv <grynkiv0x001@gmail.com>
2022-01-15 13:28:27 +0200

    First list with items functionality created

---
+++ src/api/mockData.js
@@ -0,0 +1,14 @@
+import { v4 as uuid } from 'uuid';
+
+export const itemsFromAPI = [
+  { id: uuid(), content: 'Item #1' },
+  { id: uuid(), content: 'Item #2' },
+  { id: uuid(), content: 'Item #3' },
+];
+
+export const listsFromAPI = {
+  [uuid()]: {
+    name: 'Todo',
+    items: itemsFromAPI,
+  },
+};
---
+++ src/components/Board/index.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+
+import { v4 as uuid } from 'uuid';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+
+// API
+import { itemsFromAPI, listsFromAPI } from '../../api/mockData';
+
+// Styles
+import './Board.scss';
+
+export const Board = () => {
+  const [lists, setLists] = useState(listsFromAPI);
+
+  return (
+    <div className="board">
+      <DragDropContext>
+        { Object.entries(lists).map(([id, list]) => {
+          return (
+            <Droppable key={id} droppableId={id}>
+              { (provided, snapshot) => {
+                return (
+                  <div
+                    className="board__list"
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                  >
+                    { list.items?.map((item, index) => {
+                      return (
+                        <Draggable key={item.id} draggableId={item.id} index={index}>
+                          { (provided, snapshot) => {
+                            return (
+                              <div
+                                className="board__item"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                {item.content}
+                              </div>
+                            );
+                          } }
+                        </Draggable>
+                      );
+                    }) }
+                  </div>
+                );
+              } }
+            </Droppable>
+          );
+        }) }
+      </DragDropContext>
+    </div>
+  );
+};
